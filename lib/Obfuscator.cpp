0320a26a351d307b88ebff567fa062849cf5db0b
dancing4am <dancing4am@gmail.com>
2024-07-29 18:44:59 -0400

    initial cmake test with practically empty Obfuscator.cpp

---
+++ lib/Obfuscator.cpp
@@ -0,0 +1,59 @@
+//=======================================================================
+// FILE:
+//	Obfuscator.cpp
+//
+// DESCRIPTION:
+//	Splits each basic blocks in a function into two basic blocks,
+//	and link them with an unconditional branch.
+//
+// USAGE:
+//	opt -load-pass-plugin=
+//
+//=======================================================================
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+
+void printName(Function &F) {
+	errs() << "Obfuscator test running: " << F.getName() << "\n";
+}
+
+struct Obfuscator : PassInfoMixin<Obfuscator> {
+	PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+		printName(F);
+		return PreservedAnalyses::all();
+	}
+
+	static bool isRequired() { return true; }
+};
+} // namespace
+
+//=======================================================================
+// Registration
+//=======================================================================
+llvm::PassPluginLibraryInfo getObfuscatorPluginInfo() {
+	return {LLVM_PLUGIN_API_VERSION, "Obfuscator", LLVM_VERSION_STRING,
+		[](PassBuilder &PB) {
+			PB.registerPipelineParsingCallback(
+				[](StringRef Name, FunctionPassManager &FPM,
+					ArrayRef<PassBuilder::PipelineElement>) {
+					if (Name == "obfuscator") {
+						FPM.addPass(Obfuscator());
+						return true;
+					}
+					return false;
+				});
+			}};
+
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+	return getObfuscatorPluginInfo();
+}
